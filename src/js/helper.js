9ded774e7222b34152d2957a6d464b3258dc8d8f
Saba Tsomaia <tsomaiadev@gmail.com>
2026-07-03 04:36:58 +0400

    adding helper functions for random quote fetching.

---
+++ src/js/helper.js
@@ -0,0 +1,33 @@
+const quoteText = document.querySelector("#quote-text");
+const quoteAuthor = document.querySelector("#quote-author");
+
+
+// rendering the actual data to the DOM.
+export function renderQuote(quote,author) {
+    quoteText.textContent = `"${quote}"`;
+    quoteAuthor.textContent = `${author}`;
+}
+
+
+// render data to the DOM and update localStorage.
+export function saveDisplayQuote(quote, author) {
+    renderQuote(quote, author);
+    
+    localStorage.setItem("quote", quote);
+    localStorage.setItem("author", author);
+}
+
+// This function validates returned Object data
+// and updates DOM 
+export async function fetchQuoteData() 
+{
+    console.log("Fetching new quote...");
+    const data = await fetchQuote();
+        
+    if (data)
+    {
+        // Update DOM and localstorage
+        saveDisplayQuote(data.quote, data.author);
+        console.log("Saved new quote to LocalStorage");
+    }
+}
