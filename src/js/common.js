c037a51010d6ac1470947b70e8150378a698fb1f
Saba Tsomaia <tsomaiadev@gmail.com>
2026-07-03 20:36:24 +0400

    updating URL handling for header navigation

--- src/js/common.js
+++ src/js/common.js
@@ -1,14 +1,10 @@
 export function updateNavigationBar(){
-    const navLinks = document.querySelectorAll('header nav ul li a');
-    let currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('header nav ul li a');    
+    const currentPath = window.location.pathname.split('/').pop() || 'index.html';
     
-    // Let Default path be index.html and not /
-    if (currentPath === '/') {
-        currentPath = '/index.html';
-    }
-
     navLinks.forEach(link => {
         console.log(navLinks,link.getAttribute('href'));
+        
         if(currentPath.includes(link.getAttribute('href')))
         {
             link.classList.add('active')
