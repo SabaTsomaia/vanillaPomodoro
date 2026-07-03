c685fa4adbeaca171f474ef7519325dd944030f0
Saba Tsomaia <tsomaiadev@gmail.com>
2026-07-03 04:35:06 +0400

    finishing fetch quotes functionality

--- src/js/pomodoro.js
+++ src/js/pomodoro.js
@@ -1,7 +1,53 @@
 import { updateNavigationBar } from './common.js';
+import {renderQuote,saveDisplayQuote,fetchQuoteData} from './helper.js'
+import { API_KEY } from './config.js';
 
-// DOMContentLoad Event
-document.addEventListener('DOMContentLoaded', () => {
-    updateNavigationBar();
-    console.log('DOM fully loaded and parsed');
-});
+const quoteText = document.querySelector("#quote-text");
+const quoteAuthor = document.querySelector("#quote-author");
+const inspireBtn = document.querySelector('#inspire-quote-btn');
+
+updateNavigationBar(); // Function for Handling Navigation CSS.
+
+const fetchQuote = async function() {
+    try {
+
+        // Fetch Data from an api using API-key
+        const response = await fetch("https://api.api-ninjas.com/v2/randomquotes?categories=success,wisdom,inspirational", {
+        method: "GET",
+        headers: {
+            'X-Api-Key': API_KEY,
+        } 
+        })
+
+        const result = await response.json();
+        const {quote, author} = result[0];
+
+        console.log(quote,author);
+        return {quote,author}
+
+    } catch (err){
+        console.log(err)
+    }
+
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Define quote and author variables for the
+    // DRY principle
+    const quote = localStorage.getItem("quote")
+    const author = localStorage.getItem("author")
+
+    if (quote && author)
+    {
+        // if exists, render from storage
+        renderQuote(quote, author);
+        console.log("Loaded quote from LocalStorage");
+    } else 
+    {
+        // fetch new quote using Fetch and check for data function. 
+        fetchQuoteData()
+    }
+})
+
+inspireBtn.addEventListener("click",fetchQuoteData)
