b65c367b11ac468add9e2d71f14433d735a92025
Saba Tsomaia <tsomaiadev@gmail.com>
2026-07-03 04:53:31 +0400

    added import statement for fetchQuote function

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,5 +1,9 @@
+import { fetchQuote } from "./fetchService.js";
+
+
 const quoteText = document.querySelector("#quote-text");
 const quoteAuthor = document.querySelector("#quote-author");
+
 
 
 // rendering the actual data to the DOM.
@@ -7,7 +11,6 @@ export function renderQuote(quote,author) {
     quoteText.textContent = `"${quote}"`;
     quoteAuthor.textContent = `${author}`;
 }
-
 
 // render data to the DOM and update localStorage.
 export function saveDisplayQuote(quote, author) {
