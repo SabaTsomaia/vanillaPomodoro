50a1d87dbaea286cfa79018dbeaf74143d5d1eaf
Saba Tsomaia <tsomaiadev@gmail.com>
2026-07-03 05:05:00 +0400

    adding comments, explaining How API returns the data.

--- src/js/fetchService.js
+++ src/js/fetchService.js
@@ -1,10 +1,17 @@
 import { API_KEY } from "./config.js";
 
 
+/*
+
+this function fetches data from the api-ninjas API using the private API_KEY.
+
+
+*/ 
+
 export const fetchQuote = async function() {
     try {
 
-        // Fetch Data from an api using API-key
+        // Fetch Data from an api using an API-key
         const response = await fetch("https://api.api-ninjas.com/v2/randomquotes?categories=success,wisdom,inspirational", {
         method: "GET",
         headers: {
@@ -13,11 +20,30 @@ export const fetchQuote = async function() {
         })
 
         const result = await response.json();
+        // destructure and return the result 
+        /*
+        categories parameter includes keywords separated by a comma 
+        that filters quotes.
+
+        Data comes in this format:
+        [
+            {
+            quote:"Always remember, success leaves clues.",
+            author:"John Patrick Hickey",
+            work:"On The Journey To Achievement",
+                categories:[
+                    "success",
+                    "wisdom",
+                    "inspirational"
+                    ]
+            }
+        ] 
+        */
+
         const {quote, author} = result[0];
 
         console.log(quote,author);
         return {quote,author}
-
     } catch (err){
         console.log(err)
     }
